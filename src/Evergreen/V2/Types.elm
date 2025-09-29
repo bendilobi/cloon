01524090533c3a06c81a9b405c1a5beece584e92
bendilobi <dev@bendilobi.de>
2025-09-29 22:55:02 +0200

    Preparing for v2

---
+++ src/Evergreen/V2/Types.elm
@@ -0,0 +1,50 @@
+module Evergreen.V2.Types exposing (..)
+
+import Browser
+import Browser.Dom
+import Browser.Navigation
+import Lamdera
+import Time
+import Url
+
+
+type alias FrontendModel =
+    { key : Browser.Navigation.Key
+    , message : String
+    , zone : Time.Zone
+    , time : Time.Posix
+    , size : Float
+    , dateHidden : Bool
+    }
+
+
+type alias BackendModel =
+    { message : String
+    , dateHidden : Bool
+    }
+
+
+type FrontendMsg
+    = UrlClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | NoOpFrontendMsg
+    | Tick Time.Posix
+    | AdjustTimeZone Time.Zone
+    | ViewportReceived Browser.Dom.Viewport
+    | Resized Int Int
+    | DateToggled
+
+
+type ToBackend
+    = NoOpToBackend
+    | DateHiddenChanged Bool
+
+
+type BackendMsg
+    = NoOpBackendMsg
+    | ClientConnected Lamdera.SessionId Lamdera.ClientId
+
+
+type ToFrontend
+    = NoOpToFrontend
+    | NewDateHidden Bool
